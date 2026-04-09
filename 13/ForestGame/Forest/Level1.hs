5e53fe38e315a84eea6c0742e47dce81973f2191
Johannes Helmut Sirsch <jsirsch@pm.me>
2026-04-09 20:11:10 +0200

    Add level 1 forest

---
+++ 13/ForestGame/Forest/Level1.hs
@@ -0,0 +1,26 @@
+module ForestGame.Forest.Level1 where
+
+data Forest a = Exit | Trail a (Forest a) (Forest a) (Forest a) deriving (Show)
+
+level1Forest :: (Ord a, Num a) => Forest a
+level1Forest =
+    Trail
+        3
+        ( Trail
+            7
+            (Trail 3 Exit Exit Exit)
+            (Trail 4 Exit Exit Exit)
+            (Trail 5 Exit Exit Exit)
+        )
+        ( Trail
+            3
+            (Trail 3 Exit Exit Exit)
+            (Trail 9 Exit Exit Exit)
+            (Trail 5 Exit Exit Exit)
+        )
+        ( Trail
+            5
+            (Trail 3 Exit Exit Exit)
+            (Trail 4 Exit Exit Exit)
+            (Trail 1 Exit Exit Exit)
+        )
