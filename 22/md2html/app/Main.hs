c05fc0a8a4b82fada95099d36d02ff3acb049338
Johannes Helmut Sirsch <jsirsch@pm.me>
2026-04-23 19:02:51 +0200

    Add verbose message

--- 22/md2html/app/Main.hs
+++ 22/md2html/app/Main.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
-import CLI (cliParser)
+import              CLI             (Options (..), cliParser)
+import qualified    Data.Text.IO    as TIO
+import Control.Monad (when)
 
 main :: IO ()
 main = do
-    putStrLn "Hello, Haskell!"
-    result <- cliParser
-    print result
+    Options{..} <- cliParser
+    fileContent <- TIO.readFile inputFile
+    when verbose $ do
+        mapM_ print ["-------------------------------------------"
+                    ,"Parsing this input file: " ++ inputFile
+                    ,"Writing result to output file: " ++ outputFile
+                    ,"-------------------------------------------"
+                    ,"File Content:"
+                    ,show fileContent
+                    ]
